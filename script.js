06e6f252605d7c82edcb373032b50aa66b8fe34f
TAMILANDA7692 <102408488+TAMILANDA7692@users.noreply.github.com>
2026-03-31 13:08:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function goTo(section) {
-  document.getElementById(section).scrollIntoView({
-    behavior: "smooth"
+ function showPanel(id) {
+  const panels = document.querySelectorAll(".panel");
+
+  panels.forEach(panel => {
+    panel.classList.remove("active");
   });
+
+  document.getElementById(id).classList.add("active");
 }
